4f53f200086ae7fce0aa5ae7b6faff9f4f3b503f
Niyas-k10 <mohdniyas2001@gmail.com>
2026-04-18 21:19:21 +0530

    frontend setup news

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -28,7 +28,7 @@ function Register() {
 
     try {
       await axios.post(
-        "http://127.0.0.1:8000/api/users/register/",
+        "https://dental-backend-ch8e.onrender.com/api/users/register/",
         form
       );
 
